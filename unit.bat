6adf750a286a7ceb726c3fc07a8da7a9b4a41da3
winkbrace <deruiterb@gmail.com>
2014-03-11 15:50:58 +0100

    added codeception's code coverage report to unit test script

--- unit.bat
+++ unit.bat
@@ -1,5 +1,2 @@
 @echo off
-
-REM This will create a nice html page with the result at WinkForm/tests/_log/report.html
-
-vendor\bin\codecept run unit --html -vvv
+vendor\bin\codecept run unit %1 --coverage --colors --html
